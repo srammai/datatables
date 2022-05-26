425b651f13936576727dbed605755bde9db31c48
“srammai” <mars@marsconsult.nl>
2022-05-26 22:38:00 +0200

    Two datasets implemented!

---
+++ src/columns.js
@@ -0,0 +1,18 @@
+export const customersColumns = [
+    {
+      title: "Company",
+      data: "fieldData.CompanyName",
+      searchable: false,
+    },
+    { title: "StreetAddress", data: "fieldData.StreetAddress", orderable: false },
+    { title: "State", data: "fieldData.State", orderable: false },
+    { title: "Zip", data: "fieldData.Zip", orderable: false },
+  ];
+  
+  export const admissionsColumns = [
+  
+    { title: "Matric Year", data: "fieldData.MatricYear" },
+    { title: "Status", data: "fieldData.Status" },
+    { title: "Date", data: "fieldData.StatusDate"},
+    { title: "Category", data: "fieldData.Category"},
+  ];
--- src/index.js
+++ src/index.js
@@ -1,36 +1,24 @@
-//Here we're importing items we'll need. You can add other imports here.
+import { admissionsColumns, customersColumns } from "./columns";
 
+//Here we're importing items we'll need. You can add other imports here.
+import {customersColumns, admissionsColumns} from "./columns";
 var table;
 
 // exposing loadData to FileMaker Script
 window.loadData = function (json) {
   const obj = JSON.parse(json); 
   const data = obj.data; 
-  console.log(data); 
+  const type = obj.type
 
-  // TODO: Load the DataTables libraries by linking to the DataTables CDN. 
-  //TODO: Get data from FileMaker
-  //TODO: prep it for the JS.
 
-  //TODO: create the columns.
+const columns = type ==="Customers" ? customersColumns : admissionsColumns;
 
-  //TODO: Explore options of Datatables
-  //https://datatables.net/reference/option/
-  //https://datatables.net/examples/index
-  // Create the DataTable, after destroying it if already exists
   table = $("#dtable").DataTable({
     paging: true,
     pageLength: 20,
     searching: true,
     colReorder: true,
-    columns: [
-      {title: "Company", data: "fieldData.CompanyName"},
-      {title: "Address", data: "fieldData.StreetAddress", orderable: false },
-      {title: "City", data: "fieldData.City", orderable: true },
-      {title: "State", data: "fieldData.State" },
-      {title: "Zip", data: "fieldData.Zip" },
-      {title: "ModifiedTimeStamp", data: "fieldData.ModifiedTimeStamp" },
-  ],
+    columns: columns,
     data: data,
   });
 };
